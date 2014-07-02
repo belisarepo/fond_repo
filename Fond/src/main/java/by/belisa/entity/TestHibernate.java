b711d72fc77f22bed1b6eac799f122b2721e10e7
Kruglik <kruglik@PC11-1212-1.BELISA.ORG.BY>
2014-07-02 16:28:36 +0300

    add hibernate test files

---
+++ Fond/src/main/java/by/belisa/entity/TestHibernate.java
@@ -0,0 +1,37 @@
+package by.belisa.entity;
+
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.AnnotationConfiguration;
+
+public class TestHibernate {
+	
+	public static void main(String args[]){
+		SessionFactory sf = new AnnotationConfiguration().configure().buildSessionFactory();
+		Session s = sf.openSession();
+		Transaction tr = null;
+		try {
+			tr = s.beginTransaction();
+			User u = new User();
+			Anketa a = new Anketa();
+			a.setName("Вася");
+			a.setUser(u);
+			u.setAnketa(a);
+			s.save(u);
+			System.out.println(((User)s.get(User.class, 200l)));
+			
+			tr.commit();
+		} catch (HibernateException e) {
+			tr.rollback();
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			s.close();
+		}
+		
+		
+	}
+
+}
