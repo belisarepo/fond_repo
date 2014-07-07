1087210e0fd4e5e31ec7b1530d98298ad3e2d77a
Kruglik <kruglik@PC11-1212-1.BELISA.ORG.BY>
2014-07-07 14:45:38 +0300

    big trouble

--- Fond/src/main/java/by/belisa/entity/Anketa.java
+++ Fond/src/main/java/by/belisa/entity/Anketa.java
@@ -2,8 +2,10 @@
 
 import java.io.Serializable;
 
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
@@ -16,13 +18,17 @@ import org.hibernate.annotations.Parameter;
 @Entity
 @Table(name="ANKETA")
 public class Anketa implements Serializable{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -4443436266052344647L;
 	@Id
 	@Column(name = "id")  
 	@GeneratedValue(generator = "gen")  
 	@GenericGenerator(name = "gen", strategy = "foreign", parameters = @Parameter(name = "property", value = "user"))
 	private long id;
-	@OneToOne  
-	@PrimaryKeyJoinColumn
+	@OneToOne(cascade=CascadeType.ALL, fetch=FetchType.LAZY, optional=false)  
+	@PrimaryKeyJoinColumn()
 	private User user;
 	@Column(name = "surname")
 	private String surname;
--- Fond/src/main/java/by/belisa/entity/User.java
+++ Fond/src/main/java/by/belisa/entity/User.java
@@ -1,25 +1,45 @@
 package by.belisa.entity;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.OneToOne;
+import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
 
 @Entity
 @Table(name="USER_", schema="LPORTAL")
+@SequenceGenerator(name="PK", sequenceName="SEQ_USER")
 public class User implements Serializable{
+	
+	public User(){
+		this.anketa = new Anketa();
+	}
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -1053232165638553977L;
 	@Id
-//	@GeneratedValue(strategy = GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator="PK")
 	@Column(name="userid")
 	private long id;
-	@OneToOne(cascade=CascadeType.ALL, mappedBy="user")
+	@OneToOne(mappedBy="user", cascade=CascadeType.ALL, fetch=FetchType.LAZY)
 	private Anketa anketa;
+	@ManyToMany(cascade = CascadeType.ALL)
+	@JoinTable(name = "USERS_ROLES", schema="LPORTAL", joinColumns = { @JoinColumn(name = "userid") }, inverseJoinColumns = { @JoinColumn(name = "roleid") })
+	private Set<Role> roles = new HashSet<Role>();
+	
 	public long getId() {
 		return id;
 	}
@@ -31,6 +51,12 @@ public class User implements Serializable{
 	}
 	public void setAnketa(Anketa anketa) {
 		this.anketa = anketa;
+	}
+	public Set<Role> getRoles() {
+		return roles;
+	}
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
 	}
 	
 }
